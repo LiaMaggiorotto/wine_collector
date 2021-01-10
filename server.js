c80126286e0ca34f0dafb487c985e4e0ed0304ec
Lia Sexton <liamaggiorotto@gmail.com>
2021-01-10 17:59:00 -0600

    server test successful

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 // ----------------------- External
+const express = require('express');
 
 // ----------------------- Internal
 
 // ----------------------- Instanced
+const app = express();
 
 // ----------------------- Configuration
+const PORT = 3000;
 
 // ----------------------- Middleware
 
 // ----------------------- Routes
 
 // ----------------------- Listener
+app.listen(PORT, () => {
+    console.log(`Live and listening on port ${PORT}`);
+})
